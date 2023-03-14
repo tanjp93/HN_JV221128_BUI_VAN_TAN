7525872954979587fe01ba9b2a0ce6519cccdc7f
tan <buitan561993@gmail.com>
2023-03-14 11:46:07 +0700

    upfate

--- HN_JV221128_BUI_VAN_TAN/src/Exercise1/Main.java
+++ HN_JV221128_BUI_VAN_TAN/src/Exercise1/Main.java
@@ -4,27 +4,31 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+        int age;
+        do {
             Scanner scanner = new Scanner(System.in);
-
-            System.out.println("Nhập tuổi của bạn: ");
-            int age = scanner.nextInt();
-            int birthYear = 2023-age;
-
-            String[] canList = {"Canh", "Tân", "Nhâm", "Quý", "Giáp", "Ất", "Bính", "Đinh", "Mậu", "Kỷ"};
-            String[] chiList = {"Thân", "Dậu", "Tuất", "Hợi", "Tý", "Sửu", "Dần", "Mão", "Thìn", "Tỵ", "Ngọ", "Mùi"};
-
-            int canIndex = (birthYear - 1) % 10 + 1;
-            if (canIndex==10){
-                canIndex=0;
+            System.out.println("Input your age: ");
+            age = scanner.nextInt();
+            if (age<0){
+                System.out.println("Your input number is not available!");
             }
-            int chiIndex = (birthYear - 1) % 12 + 1;
-            if (chiIndex==12){
-                chiIndex=0;
-            }
-            String can = canList[canIndex];
-            String chi = chiList[chiIndex];
+        } while (age < 0);
+        int birthYear = 2023 - age;
 
-            System.out.printf("\t" + "%d → %s %s ", age, can, chi);
+        String[] canList = {"Canh", "Tân", "Nhâm", "Quý", "Giáp", "Ất", "Bính", "Đinh", "Mậu", "Kỷ"};
+        String[] chiList = {"Thân", "Dậu", "Tuất", "Hợi", "Tý", "Sửu", "Dần", "Mão", "Thìn", "Tỵ", "Ngọ", "Mùi"};
+
+        int canIndex = (birthYear - 1) % 10 + 1;
+        if (canIndex == 10) {
+            canIndex = 0;
+        }
+        int chiIndex = (birthYear - 1) % 12 + 1;
+        if (chiIndex == 12) {
+            chiIndex = 0;
+        }
+        String can = canList[canIndex];
+        String chi = chiList[chiIndex];
+
+        System.out.printf("\t" + "%d → %s %s ", age, can, chi);
     }
 }
